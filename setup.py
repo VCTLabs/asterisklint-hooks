5fe645143a4a7ab964fe0087e5343b040caf00d9
Stephen L Arnold <nerdboy@gentoo.org>
2022-11-29 09:49:22 -0800

    chg: dev: update packaging and tool configs
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
+from pathlib import Path
+
 from setuptools import setup
+from versioningit import get_cmdclasses
 
-
-with open(".version", 'r') as f:
-    AL_VERSION = f.read().strip()
+AL_VERSION = Path(__file__).with_name(".version").read_text().strip()
 
 
 setup(
-    install_requires=[f'asterisklint=={AL_VERSION}'],
+    cmdclass=get_cmdclasses(),
+    name="asterisklint_hooks",
+    version="{}".format(AL_VERSION),
+    install_requires=["asterisklint=={}".format(AL_VERSION)],
 )

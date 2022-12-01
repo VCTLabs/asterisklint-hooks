15ebb6d2b4a31f6c3739c6cd970038377da106a8
Stephen L Arnold <nerdboy@gentoo.org>
2022-11-30 17:09:41 -0800

    fix: remove versioningit dep, simplify build requirements
    * setup.py and pip installs were broken using versioningit onbuild cfg
    * stop trying to manage version, add ci workflow, rename version file
    * update packaging and workflows, use (mirror) tag version
    * no windows in ci matrix => dir-full-of-symlinks install fail
    
    Signed-off-by: Stephen L Arnold <nerdboy@gentoo.org>

--- setup.py
+++ setup.py
@@ -3,14 +3,13 @@ from __future__ import annotations
 from pathlib import Path
 
 from setuptools import setup
-from versioningit import get_cmdclasses
 
-AL_VERSION = Path(__file__).with_name(".version").read_text().strip()
+AL_VERSION = Path(__file__).with_name("version.txt").read_text().strip()
 
 
-setup(
-    cmdclass=get_cmdclasses(),
-    name="asterisklint_hooks",
-    version="{}".format(AL_VERSION),
-    install_requires=["asterisklint=={}".format(AL_VERSION)],
-)
+if __name__ == "__main__":
+    setup(
+        name="asterisklint_hooks",
+        version="{}".format(AL_VERSION),
+        install_requires=["asterisklint=={}".format(AL_VERSION)],
+    )
